85ccdafedf23bec6a55ee89665d48981d2154f28
Robin9800 <robin.costanzo@gmail.com>
2022-03-07 16:26:14 +0100

    "visible" & "notVisible" added

--- teachersboolean-ex-hamburger-menu-base-6862becc9f78/main.js
+++ teachersboolean-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -1,17 +1,17 @@
 /*Si dichiare la costante associata alla classe ".fa-bars" che apre il menu*/
-const menuOn = document.querySelector('.fa-bars');
+const menuOn = document.querySelector(".fa-bars");
 
 menuOn.addEventListener("click", function() {
-  let visible = document.querySelector('.hambuerger-menu');
-    menuOn.style.display = "block";
+    let visible = document.querySelector(".hamburger-menu");
+    visible.style.display = "block";
 });
 
 
 /*Si dichiare la costante associata alla classe ".fa-times" che chiude il menu*/
-const menuOff = document.querySelector('.fa-times');
+const menuOff = document.querySelector(".fa-times");
 
 menuOff.addEventListener("click", function(){
-    let notVisible = document.querySelector('.hambuerger-menu');
-    menuOff.style.display = "none";
+    let notVisible = document.querySelector(".hamburger-menu");
+    notVisible.style.display = "none";
 });
 
